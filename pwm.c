6d069815e8c929421e75364ced3437f8977f25cb
Stavros Filargyropoulos <stav@stav.cc>
2022-08-26 06:26:48 +0100

    .

--- pwm.c
+++ pwm.c
@@ -1,5 +1,7 @@
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 #include "bcm2835.h"
 
@@ -7,7 +9,51 @@
 #define PWM_CHANNEL 0
 #define RANGE 48
 
-int main(int argc, char **argv)
+int gettemp (void)
+{
+  int f;
+  char buf[128];
+  int n;
+  int temp;
+  
+  f = open("/sys/class/thermal/thermal_zone0/temp", O_RDONLY);
+  if (f == -1) {
+    printf("Failed opening temp file\n");
+    return 100;
+  }
+
+  n = read(f, buf, sizeof(buf) - 1);
+  if (n < 0) {
+    printf("Failed reading temp from file\n");
+    return 100;
+  }
+  buf[n] = '\0';
+
+  temp = atoi(buf);
+  temp = temp / 1000;
+  if (temp < 0 || temp > 100) {
+    return 100;
+  }
+
+  return temp;
+}
+
+int
+getfanpercentage (int temp)
+{
+  if (temp < 20)
+    return 0;
+
+  if (temp > 80)
+    return 100;
+
+  temp -= 20;
+  
+  return (temp * 100) / 50;
+}
+
+int
+main (int argc, char **argv)
 {
     if (!bcm2835_init())
 	return 1;
@@ -22,7 +68,17 @@ int main(int argc, char **argv)
     bcm2835_pwm_set_mode(PWM_CHANNEL, 1, 1);
     bcm2835_pwm_set_range(PWM_CHANNEL, RANGE);
 
-    bcm2835_pwm_set_data(PWM_CHANNEL, 6);
+    while (1) {
+      int temp = gettemp();
+      int percentage;
+      int value;
+      percentage = getfanpercentage(temp);
+      value = (percentage * RANGE) / 100;
+      printf("Temp: %d, Fan%%: %d, Value: %d/%d\n",
+	     temp, percentage, value, RANGE);
+      bcm2835_pwm_set_data(PWM_CHANNEL, value);
+      usleep(1 * 1000 * 1000);
+    }
 
     bcm2835_close();
     return 0;
